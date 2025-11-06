72da9d668e07a0bfe5c4b259d3b07dd58cc2fbef
cordi2010 <128414808+cordi2010@users.noreply.github.com>
2025-11-06 14:53:24 +0000

    Update testspace.py
    testing changing chest code to be a function to simplify the code and make everything easier

--- testspace.py
+++ testspace.py
@@ -1,5 +1,6 @@
 from Items import chestia
 from Items import chestia2
+from Items import chestia3
 from rn import doorhealthrandom
 from rn import skinrandom
 from rn import clawrandom
@@ -20,11 +21,100 @@ healthdoor = doorhealthrandom
 uc = creaturerandom
 attack = "yes"
 armourlvl = 0
+armourslot = " "
+weaponsslot = " "
+itemsslots = []
+
+
+def inventory():
+    print("armour ", armourslot)
+    print("weapon ", weaponsslot)
+    print("items ", itemsslots)
+def chestfunction():
+        chest = input("do you want to open the chest ")
+    print("")
+
+    invalid = True
+    if chest == "yes" or chest == "no":
+        invalid = False
+
+    # more input validation
+    while invalid == True:
+        print("wrong input")
+        chest = input("yes or no?")
+        if chest == "yes" or chest == "no":
+            invalid = False
+        else:
+            continue
+
+    if chest == "no":
+        print("you headbutt the chest")
+        exit()
+    elif chest == "yes":
+
+        print(f"you open the chest inside is {chestia}")
+        if chestia == "a sword" or chestia == "a mace" or chestia == "an axe" or chestia == "a spear":
+            punch = punch + 4
+            weponsslot = chestia
+            print(f"you do {punch} damage now")
+
+        elif chestia == "bread" or chestia == "an apple" or chestia == "a cookie":
+            restorehealth = input("do you want to eat this and restore your health? ")
+
+            invalid = True
+            if restorehealth == "yes" or restorehealth == "no":
+                invalid = False
+
+            # more input validation
+            while invalid == True:
+                print("wrong input")
+                restorehealth = input("yes or no?")
+                if restorehealth == "yes" or restorehealth == "no":
+                    invalid = False
+                else:
+                    continue
+
+            if restorehealth == "yes":
+                playerhealth = 20
+                print("player health is now ", playerhealth)
+            elif restorehealth == "no":
+                storeitem = input("do you want to put this item in your inventory? ")
+
+                invalid = True
+                if storeitem == "yes" or storeitem == "no":
+                    invalid = False
+
+                # more input validation
+                while invalid == True:
+                    print("wrong input")
+                    storeitem = input("yes or no?")
+                    if storeitem == "yes" or storeitem == "no":
+                        invalid = False
+                    else:
+                        continue
+                if storeitem == "yes":
+                    itemsslots.append(chestia)
+                    print("the item is now in your inventory ")
+                elif storeitem == "no":
+                    print("you leave the item where you found it ")
+
+        elif chestia == "iron armour" or chestia == "leather armour" or chestia == "diamond armour":
+            if chestia == "leather armour":
+                armourlvl = armourlvl + 1
+                armourslot = chestia
+            elif chestia == "iron armour":
+                armourlvl = armourlvl + 2
+                armourslot = chestia
+            elif chestia == "diamond armour":
+                armourlvl = armourlvl + 3
+                armourslot = chestia
+
+
 print(" ")
 print("Welcome to THE DOOR, a game created by Corin Dishon and Eli Wood")
 print(f"{playerhealth} health")
 print(" ")
-invalid = True
+
 
 while healthdoor > 0:
     print("there is a door")
@@ -32,7 +122,11 @@ while healthdoor > 0:
     # ptd = Punch The Door
     ptd = input("do you want to punch? ")
 
+    invalid = True
     if ptd == "yes" or ptd == "no" or ptd == "ptd":
+        invalid = False
+    elif ptd == "inventory":
+        inventory()
         invalid = False
 
     # more input validation
@@ -53,8 +147,7 @@ while healthdoor > 0:
         healthdoor = healthdoor - punch
         print(f"you did {punch} damage")
         print("door health ", healthdoor)
-    else:
-        continue
+
 print(" ")
 print("you destroyed it")
 print(" ")
@@ -64,6 +157,9 @@ print(" ")
 
 invalid = True
 if laatd == "yes" or laatd == "no":
+    invalid = False
+elif laatd == "inventory":
+    inventory()
     invalid = False
 
 # more input validation
@@ -77,78 +173,10 @@ while invalid == True:
 
 if laatd == "yes":
     print("you enter the room inside is a chest")
-    chest = input("do you want to open the chest ")
-    print("")
-
-    invalid = True
-    if chest == "yes" or chest == "no":
-        invalid = False
-
-    # more input validation
-    while invalid == True:
-        print("wrong input")
-        chest = input("yes or no?")
-        if chest == "yes" or chest == "no":
-            invalid = False
-        else:
-            continue
-
-        if chest == "n":
-            print("you headbutt the chest")
-            exit()
-        else:
-            continue
-print(f"you open the chest inside is {chestia}")
-if chestia == "a sword" or chestia == "a mace" or chestia == "an axe" or chestia == "a spear":
-    punch = punch + 4
-    print(f"you do {punch} damage now")
-elif chestia == "bread" or chestia == "an apple" or chestia == "a cookie":
-    restorehealth = input("do you want to eat this and restore your health? ")
-
-    invalid = True
-    if restorehealth == "yes" or restorehealth == "no":
-        invalid = False
-
-    # more input validation
-    while invalid == True:
-        print("wrong input")
-        restorehealth = input("yes or no?")
-        if restorehealth == "yes" or restorehealth == "no":
-            invalid = False
-        else:
-            continue
-
-    if restorehealth == "yes":
-        playerhealth = 20
-        print("player health is now ", playerhealth)
-    elif restorehealth == "no":
-        leaveitem = input("do you want to leave this item? ")
-
-        invalid = True
-        if leaveitem == "yes" or leaveitem == "no":
-            invalid = False
-
-        # more input validation
-        while invalid == True:
-            print("wrong input")
-            leaveitem = input("yes or no?")
-            if leaveitem == "yes" or leaveitem == "no":
-                invalid = False
-            else:
-                continue
-        if leaveitem == "yes":
-            print("you decide not to be a theif and leave the item")
-        elif leaveitem == "no":
-            print("you were so indecisive that you passed out")
-        
-elif chestia == "iron armour" or chestia == "leather armour" or chestia == "diamond armour":
-    if chestia == "leather armour":
-        armourlvl = armourlvl + 1
-    elif chestia == "iron armour":
-        armourlvl = armourlvl + 2
-    elif chestia == "diamond armour":
-        armourlvl = armourlvl + 3
-
+    chestfunction()
+elif laatd == "no":
+    print("you turn around and leave ")
+    exit
 
 print("You turn around and leave")
 print(" ")
