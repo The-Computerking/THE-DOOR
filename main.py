92bc38389a5b93729b683a28a98bc9cc4c4a7840
cordi2010 <128414808+cordi2010@users.noreply.github.com>
2025-11-06 15:23:59 +0000

    Update main.py
    started changing over to using functions

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ from descrypters import clawsharpness
 from descrypters import claws
 from random import randint
 import subprocess
+from functions import inputvalidationdoor
+from functions import inputvalidationchest
+from functions import inputvalidationlookaround
+from functions import chestfunction
+
 
 playerhealth = 20
 punch = 1
@@ -45,21 +50,7 @@ while healthdoor > 0:
     # ptd = Punch The Door
     ptd = input("do you want to punch? ")
 
-    invalid = True
-    if ptd == "yes" or ptd == "no" or ptd == "ptd":
-        invalid = False
-    elif ptd == "inventory":
-        inventory()
-        invalid = False
-
-    # more input validation
-    while invalid == True:
-        print("wrong input")
-        ptd = input("yes or no? ")
-        if ptd == "yes" or ptd == "no" or ptd == "ptd":
-            invalid = False
-        else:
-            continue
+    inputvalidationdoor()
 
     if ptd == "no":
         print("You Turn Around and Leave")
@@ -78,101 +69,17 @@ print(" ")
 laatd = input("beyond the door you see a room, Do you want to look around? ")
 print(" ")
 
-invalid = True
-if laatd == "yes" or laatd == "no":
-    invalid = False
-elif laatd == "inventory":
-    inventory()
-    invalid = False
-
-# more input validation
-while invalid == True:
-        print("wrong input")
-        laatd = input("yes or no ")
-        if laatd == "yes" or laatd == "no":
-            invalid = False
-        else:
-            continue
+inputvalidationlookaround
 
 if laatd == "yes":
     print("you enter the room inside is a chest")
     chest = input("do you want to open the chest ")
     print("")
 
-    invalid = True
-    if chest == "yes" or chest == "no":
-        invalid = False
+    inputvalidationchest()
 
-    # more input validation
-    while invalid == True:
-        print("wrong input")
-        chest = input("yes or no?")
-        if chest == "yes" or chest == "no":
-            invalid = False
-        else:
-            continue
+    chestfunction()
 
-    if chest == "no":
-        print("you headbutt the chest")
-        exit()
-    elif chest == "yes":
-
-        print(f"you open the chest inside is {chestia}")
-        if chestia == "a sword" or chestia == "a mace" or chestia == "an axe" or chestia == "a spear":
-            punch = punch + 4
-            weponsslot = chestia
-            print(f"you do {punch} damage now")
-
-        elif chestia == "bread" or chestia == "an apple" or chestia == "a cookie":
-            restorehealth = input("do you want to eat this and restore your health? ")
-
-            invalid = True
-            if restorehealth == "yes" or restorehealth == "no":
-                invalid = False
-
-            # more input validation
-            while invalid == True:
-                print("wrong input")
-                restorehealth = input("yes or no?")
-                if restorehealth == "yes" or restorehealth == "no":
-                    invalid = False
-                else:
-                    continue
-
-            if restorehealth == "yes":
-                playerhealth = 20
-                print("player health is now ", playerhealth)
-            elif restorehealth == "no":
-                storeitem = input("do you want to put this item in your inventory? ")
-
-                invalid = True
-                if storeitem == "yes" or storeitem == "no":
-                    invalid = False
-
-                # more input validation
-                while invalid == True:
-                    print("wrong input")
-                    storeitem = input("yes or no?")
-                    if storeitem == "yes" or storeitem == "no":
-                        invalid = False
-                    else:
-                        continue
-                if storeitem == "yes":
-                    itemsslots.append(chestia)
-                    print("the item is now in your inventory ")
-                elif storeitem == "no":
-                    print("you leave the item where you found it ")
-
-        elif chestia == "iron armour" or chestia == "leather armour" or chestia == "diamond armour":
-            if chestia == "leather armour":
-                armourlvl = armourlvl + 1
-                armourslot = chestia
-            elif chestia == "iron armour":
-                armourlvl = armourlvl + 2
-                armourslot = chestia
-            elif chestia == "diamond armour":
-                armourlvl = armourlvl + 3
-                armourslot = chestia
 elif laatd == "no":
     print("you turn around and leave ")
     exit
@@ -192,22 +99,7 @@ while healthdoor > 0:
     ptd = input("do you want to punch? ")
     print(" ")
 
-    invalid = True
-    if ptd == "yes" or ptd == "no" or ptd == "ptd":
-        invalid = False
-    elif ptd == "inventory":
-        inventory()
-        invalid = False
-
-    # more input validation
-    while invalid == True:
-        print("wrong input")
-        ptd = input("yes or no? ")
-        print(" ")
-        if ptd == "yes" or ptd == "no" or ptd == "ptd":
-            invalid = False
-        else:
-            continue
+    inputvalidationdoor()
 
     if ptd == "no":
         print("You Turn Around and Leave")
@@ -227,21 +119,7 @@ print(" ")
 laatd = input("do you want to enter? ")
 print(" ")
 
-invalid = True
-if laatd == "yes" or laatd == "no":
-    invalid = False
-elif laatd == "inventory":
-    inventory()
-    invalid = False
-
-# more input validation
-while invalid == True:
-    print("wrong input")
-    laatd = input("yes or no?")
-    if laatd == "yes" or laatd == "no":
-        invalid = False
-    else:
-        continue
+inputvalidationlookaround()
 
 skin = skin
 skinrandom = skinrandom
@@ -254,25 +132,11 @@ clawsharpnessrandom = clawsharpnessrandom
 
 if laatd == "yes":
     print("you enter the room")
-    investigate = input("do you want to investigate? ")
+    ptd = input("do you want to investigate? ")
 
-    invalid = True
-    if investigate == "yes" or investigate == "no":
-        invalid = False
-    elif investigate == "inventory":
-        inventory()
-        invalid = False
+    inputvalidationchest()
 
-    # more input validation
-    while invalid == True:
-        print("wrong input ")
-        investigate = input("yes or no? ")
-        if investigate == "yes" or investigate == "no":
-            invalid = False
-        else:
-            continue
-
-    if investigate == "yes":
+    if ptd == "yes":
         print(" ")
         print(f"you approach the creature and look closer its got {skin[skinrandom]} skin")
         print(f"and {teeth[skinrandom]} teeth and {claws[clawrandom]} {clawsharpness[clawsharpnessrandom]} claws")
@@ -362,7 +226,7 @@ if laatd == "yes":
             elif attack == "ptd":
                 uc = uc - 20
 
-    elif investigate == "no":
+    elif ptd == "no":
         print("you did not approach the creature")
         print("you leave")
         print(" ")
